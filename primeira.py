6435e9ec648e5ec23cad675625a8294a0bcb02a9
CauanBueno <cauanbueno98@gmail.com>
2023-12-11 22:09:48 -0300

    Primeiros códigos

---
+++ primeira.py
@@ -0,0 +1,43 @@
+# Texto - String ""
+# Numero - Int(Interger) 0
+#nome = input("Qual é o seu nome? - ")
+#anoNascimento = input("Qual ano você nasceu? - ")
+#idade = 2023 - int(anoNascimento)
+
+#Contatenação
+#print("Olá " + nome + "! Você tem " + str(idade) + " de idade.")
+
+##Formatted strings
+#print(f"Olá {nome}! Você tem {str(idade)} de idade")
+
+#mensagem = "Eu amo paçoca!"
+# mensagem. me da opções para formatar o texto
+#print(mensagem.upper)
+
+#Calculos
+
+#soma = 1 + 1
+#divisao = 3/3
+#calculo = (2 + 2) * 3 / 2 - 10
+#print(calculo)
+
+#Comparação
+# == igual
+# != diferente
+# > maior que
+# < menor que
+# >= Maior ou igual que
+# <= Menor ou igual que
+
+valorA = 10
+valorB = 20
+frutaA = "Banana"
+frutaB = "banana"
+
+if frutaA != frutaB:
+    print("As frutas não são iguais")
+else:
+    print("As frutas são iguais")
+
+#operacao = frutaA == frutaB
+#print(operacao)
